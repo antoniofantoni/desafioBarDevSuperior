92362980bc59b5fb27f8913fa3a543772b4cfaf2
Antonio Fantoni <94508080+antoniofantoni@users.noreply.github.com>
2022-12-07 14:51:02 -0300

    Update bar.js
    Commit de Refatoração

--- bar.js
+++ bar.js
@@ -1,10 +1,9 @@
-const valorCerveja = 5;
-const valorRefrigerante = 3;
-const valorEspetinho = 7;
-var couvertArtistico = 4;
-const ingressoM = 10;
-const ingressoF=8; 
-
+let valorCerveja = 5;
+let valorRefrigerante = 3;
+let valorEspetinho = 7;
+let couvertArtistico = 4;
+let valorDaConta=0;
+let ingresso=0;
 
 function calculaConsumoNoBar(){
 
@@ -12,62 +11,40 @@ function calculaConsumoNoBar(){
     let qtdeDeCervejas = parseInt(document.getElementById("qtdeDeCervejas").value);
     let qtdeDeRefrigerantes = parseInt(document.getElementById("qtdeDeRefrigerantes").value);
     let qtdeDeEspetinhos = parseInt(document.getElementById("qtdeDeEspetinhos").value);
-
+    let consumo = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho)  
+   
     document.getElementById("sexo").innerHTML =  `Sexo: ${sexo}`
     document.getElementById("cerveja").innerHTML =  `Quantidade de cervejas: ${qtdeDeCervejas}`
     document.getElementById("refri").innerHTML =  `Quantidade de Refrigerantes: ${qtdeDeRefrigerantes}`
     document.getElementById("espetos").innerHTML =  `Quantidade de espetinhos: ${qtdeDeEspetinhos}`
-
-    
-    if(sexo==="M") {
-        
-            
-    let consumo = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho)
-
-
-
+   
+    if (consumo > 30  &&  sexo==="M") {
+        ingresso=10
+        valorDaConta=consumo+ingresso
+        document.getElementById("couvert").innerHTML =  "Isento de couvert"
+               
+    } else {
+        if(consumo <= 30  &&  sexo==="M"){
+            ingresso = 10
+            valorDaConta=consumo+ingresso+couvertArtistico
+            document.getElementById("couvert").innerHTML =  `Couvert = R$ ${couvertArtistico.toFixed(2)}`
+        } else {
+                if(consumo > 30  &&  sexo==="F") {
+                    ingresso=8
+                    valorDaConta=consumo+ingresso
+                    document.getElementById("couvert").innerHTML =  "Isento de couvert"
+                } else {
+                    ingresso=8
+                    valorDaConta=consumo+ingresso+couvertArtistico 
+                    document.getElementById("couvert").innerHTML =  `Couvert = R$ ${couvertArtistico.toFixed(2)}`
+            }
+        }
+    }
     document.getElementById("relatorio").innerHTML =  "RELATÓRIO:"
     document.getElementById("consumo").innerHTML =  `Consumo: R$ ${consumo.toFixed(2)}`
-
-    if (consumo >30) {
-        
-        let valorDaConta = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho) + ingressoM
-        document.getElementById("couvert").innerHTML =  "Isento de couvert"
-        document.getElementById("ingresso").innerHTML =  `ingresso: R$ ${ingressoM.toFixed(2)}`
-        document.getElementById("conta").innerHTML =  `Valor a pagar: R$ ${valorDaConta.toFixed(2)}`  
-    }else {
-        
-        let valorDaConta = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho) + ingressoM + couvertArtistico
-
-        document.getElementById("couvert").innerHTML =  `Couvert = R$ ${couvertArtistico.toFixed(2)}`
-        document.getElementById("ingresso").innerHTML =  `ingresso: R$ ${ingressoM.toFixed(2)}`
-        document.getElementById("conta").innerHTML =  `Valor a pagar: R$ ${valorDaConta.toFixed(2)}` 
-    }
-    
-    }else { //F - else do if se é do sexo Masculino
-
-        let consumo = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho)
-
-    document.getElementById("relatorio").innerHTML =  "RELATÓRIO:"
-    document.getElementById("consumo").innerHTML =  `Consumo: R$ ${consumo.toFixed(2)}`
-
-    if (consumo >30) {
-        
-        let valorDaConta = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho) + ingressoF
-        document.getElementById("couvert").innerHTML =  "Isento de couvert"
-        document.getElementById("ingresso").innerHTML =  `ingresso: R$ ${ingressoF.toFixed(2)}`
-        document.getElementById("conta").innerHTML =  `Valor a pagar: R$ ${valorDaConta.toFixed(2)}`  
-    }else {
-        
-        let valorDaConta = (valorCerveja * qtdeDeCervejas) + (valorRefrigerante * qtdeDeRefrigerantes) + (qtdeDeEspetinhos * valorEspetinho) + ingressoF + couvertArtistico
-
-        document.getElementById("couvert").innerHTML =  `Couvert = R$ ${couvertArtistico.toFixed(2)}`
-        document.getElementById("ingresso").innerHTML =  `ingresso: R$ ${ingressoF.toFixed(2)}`
-        document.getElementById("conta").innerHTML =  `Valor a pagar: R$ ${valorDaConta.toFixed(2)}`
-    }
-    }
+    document.getElementById("ingresso").innerHTML =  `ingresso: R$ ${ingresso.toFixed(2)}`
+    document.getElementById("conta").innerHTML =  `Valor a pagar: R$ ${valorDaConta.toFixed(2)}`    
 }
-
 
 
 
